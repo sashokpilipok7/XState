7812fd681e39c17769b2847a648758e956cb7b78
SashaPilipenko__ <sashapylypenko.pov@gmail.com>
2021-05-07 15:56:12 +0300

    init: some reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const myReducer = (state, event) => {
+  switch(event.type){
+    case "EDIT":
+      return {
+        ...state,
+        status: 'editing'
+      };
+    case "CANCEL":
+      return {
+        ...state,
+        status: 'reading'
+      };
+    case "COMMIT":
+      return {
+        ...state,
+        value: event.value,
+        status: 'reading'
+      };
+      default:
+        return state;
+  }
+}
+
 function App() {
+  const [state, send] = useReducer(myReducer, {
+    status: 'editing',
+    value: 'Hello fwdays'
+  })
+  const { status , value } = state;
+  console.log(status, 'status')
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +47,8 @@ function App() {
         >
           Learn React
         </a>
+        {status === 'editing' ? <p onClick={() => send({type: 'CANCEL'})}>{state.status}</p> : <input value="input" />}
+        
       </header>
     </div>
   );
